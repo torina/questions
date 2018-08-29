cb3e3a9dbb3c419efe3cae5c4c8ee1ca51dee354
vbula <viktoriia.bula@1und1.de>
2018-08-29 18:13:27 +0200

    init

--- src/test/java/ua/tools/questions/QuestionsApplicationTests.java
+++ src/test/java/ua/tools/questions/QuestionsApplicationTests.java
@@ -1,16 +1,19 @@
 package ua.tools.questions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class QuestionsApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	    log.debug("context loads");
 	}
 
 }
