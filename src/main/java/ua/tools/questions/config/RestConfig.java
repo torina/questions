b1a0b3c8d08851d74d0b28b81c47cfa30de74e16
torina <Eternalove_4>
2018-10-14 15:02:11 +0200

    add rest impl

---
+++ src/main/java/ua/tools/questions/config/RestConfig.java
@@ -0,0 +1,18 @@
+package ua.tools.questions.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+
+}
